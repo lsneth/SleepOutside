b14d09fa1075b6182c4781fba2ae74afce085cb0
Lucas Nethercott <luke@nethercott.com>
2023-05-22 16:16:08 -0600

    finish up assignment

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,5 +1,6 @@
 import productList from './productList.mjs';
 import { getParam } from './utils.mjs';
-let category = getParam('category');
+let category = getParam('category') ?? 'tents';
 
-productList((category = 'tents'), '.product-list');
+document.querySelector('.page-title').innerHTML = `Top Products: ${category}`;
+productList(category, '.product-list');
--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -3,9 +3,9 @@ import {renderListWithTemplate} from './utils.mjs'
 
 function productCardTemplate(product){
     return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+    <a href="../product_pages/index.html?product=${product.Id}">
       <img
-        src="../${product.Image}" alt="${product.Name}"
+        src="${product.Images.PrimaryMedium}" alt="${product.Name}"
       />
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
