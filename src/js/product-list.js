d28dbe7241d6b4948aeb215a9e07d7e7ea290fd4
Lucas Nethercott <luke@nethercott.com>
2023-05-23 06:25:50 -0600

    final fixes

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -2,5 +2,5 @@ import productList from './productList.mjs';
 import { getParam } from './utils.mjs';
 let category = getParam('category') ?? 'tents';
 
-document.querySelector('.page-title').innerHTML = `Top Products: ${category}`;
+document.querySelector('.page-title').textContent = `Top Products: ${category}`;
 productList(category, '.product-list');
